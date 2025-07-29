4c34c3b994536c3e974397dca6e79d4ba8915cf8
jeromeku <jerome.ku@gmail.com>
2025-07-29 01:18:41 +0000

    working for TP / EP / ETP

--- experiments/convert_hf_to_mcore.sh
+++ experiments/convert_hf_to_mcore.sh
@@ -6,11 +6,11 @@ QWEN3_MOE="assets/qwen3_moe_4layer_16experts" #"assets/qwen3_moe_4layer" #"Qwen/
 
 MODEL_ID="${QWEN3_MOE}"
 
-TP=2
+TP=4
 PP=1
 CP=1
 EP=2
-ETP=1
+ETP=2
 VPP_SIZE=None
 
 WORLD_SIZE_NON_MOE=$((TP * CP * PP))
